909727be58d36d28747548cb12a2657f6c9c664a
Robin Lemaire <robin.lemaire@adevinta.com>
2025-10-17 15:19:54 +0200

    [SwiftUI] Improve theme in EnvironmentValues

--- Sources/Core/SwiftUI/Environment/ThemeEnvironmentValues.swift
+++ Sources/Core/SwiftUI/Environment/ThemeEnvironmentValues.swift
@@ -11,22 +11,33 @@ import SparkTheming
 import SwiftUI
 
 public extension EnvironmentValues {
-    @Entry var theme: ThemeDefault = RainbowTheme.shared
+    @Entry var theme: EnvironmentTheme = .init(value: RainbowTheme.shared)
 }
 
 public extension View {
 
-    /// Set the **theme** (only work if the Theme is init with the ```ThemeDefault```).
+    /// Set the **theme**.
     ///
     /// Each components which using ```@Environment(\.theme) private var theme``` will be automatically updated.
     ///
     /// The default value is *RainbowTheme.shared*.
     @ViewBuilder
     func sparkTheme(_ theme: any Theme) -> some View {
-        if let theme = theme as? ThemeDefault {
-            self.environment(\.theme, theme)
-        } else {
-            self
-        }
+        self.environment(\.theme, .init(value: theme))
+    }
+}
+
+/// Used only on Spark component ```@Environment(\.theme)```.
+/// Set only by the **theme** on **EnvironmentValues**.
+public struct EnvironmentTheme: Equatable {
+
+    // MARK: - Properties
+
+    public let value: any Theme
+
+    // MARK: - Equatable
+
+    public static func == (lhs: EnvironmentTheme, rhs: EnvironmentTheme) -> Bool {
+        lhs.value.equals(rhs.value)
     }
 }
